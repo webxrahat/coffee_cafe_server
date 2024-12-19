bc6293bca601c67901f82bf861ad31fce1b140f4
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-19 22:54:26 +0600

    set

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require("express");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.port || 5000;
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.jhexa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+ serverApi: {
+  version: ServerApiVersion.v1,
+  strict: true,
+  deprecationErrors: true,
+ },
+});
+
+async function run() {
+ try {
+  await client.connect();
+
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+ } finally {
+ }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
  res.send("Hello World!");
