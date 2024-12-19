3c4d9b572991fca35561105b6b56b2d352b184f9
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-19 08:14:33 +0600

    gg

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//index
+const express = require("express");
+const app = express();
+const port = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+ res.send("Hello World!");
+});
+
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`);
+});
